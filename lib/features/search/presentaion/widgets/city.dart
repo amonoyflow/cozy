b9e2dae0fc4c08dc57a498652194e33d9ac4d6dd
Florante Amonoy <flow@Florantes-MacBook-Pro.local>
2020-09-06 17:36:57 +0800

    Added more UI on search screen

---
+++ lib/features/search/presentaion/widgets/city.dart
@@ -0,0 +1,43 @@
+import 'package:cozy/features/search/presentaion/bloc/search_bloc.dart';
+import 'package:cozy/features/search/presentaion/widgets/city_autocomplete_search.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class City extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<SearchBloc, SearchState>(builder: (context, state) {
+      if (state is CitySearchLoaded) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 13.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Location",
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  Text(
+                    "${state.city}",
+                    style: TextStyle(
+                      fontSize: 14.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 15.0),
+            CityAutoCompleteSearch(),
+          ],
+        );
+      }
+      return Container();
+    });
+  }
+}
